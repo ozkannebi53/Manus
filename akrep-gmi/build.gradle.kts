acfd8b60510c285a02cc4d58a98c1e054d829c0f
AKREP GMİ <akrep@gmi.com>
2026-06-09 13:13:57 +0000

    Update Kotlin version to 1.9.24 for Compose Compiler compatibility

--- akrep-gmi/build.gradle.kts
+++ akrep-gmi/build.gradle.kts
@@ -3,8 +3,8 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("com.android.library") version "8.1.0" apply false
-    kotlin("android") version "1.9.0" apply false
-    kotlin("plugin.serialization") version "1.9.0" apply false
+    kotlin("android") version "1.9.24" apply false
+    kotlin("plugin.serialization") version "1.9.24" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
